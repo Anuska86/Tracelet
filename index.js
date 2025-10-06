5ac0cb9f4d2a5ff12b2509d6fbe29ef5a7cc834a
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-10-06 15:55:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+let myLeads = [];
 let inputBtn = document.getElementById("input-btn");
+let inputEl = document.getElementById("input-el");
 
 inputBtn.addEventListener("click", function () {
   console.log("Button clicked!");
