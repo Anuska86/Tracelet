d3bfad484dfefdaa5ff2f968bd98868c3975f727
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-10-07 11:44:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ inputBtn.addEventListener("click", function () {
 
 function renderLeads() {
   for (i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems +=
+      "<li><a href='" +
+      myLeads[i] +
+      "' target='_blank'>" +
+      myLeads[i] +
+      "</a></li>";
   }
   return (ulEl.innerHTML = listItems);
 }
