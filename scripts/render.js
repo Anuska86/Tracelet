5c98dc35536929f890c1d99feb25a56f2096d64b
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-10-09 15:25:08 +0200

    delete tabs

--- scripts/render.js
+++ scripts/render.js
@@ -1,4 +1,3 @@
-// scripts/render.js
 export function renderLeads(leads, container) {
   const grouped = leads.reduce((acc, lead) => {
     if (!lead.category) return acc;
@@ -13,18 +12,26 @@ export function renderLeads(leads, container) {
     container.innerHTML += `<h3>${category}</h3><ul>`;
     grouped[category].forEach((lead) => {
       container.innerHTML += `
-        <li>
-          <a target="_blank" href="${lead.url}">${lead.url}</a>
-          ${
-            lead.description
-              ? `<p class="description">${lead.description}</p>`
-              : ""
-          }
-          <span class="timestamp">${lead.timestamp}</span>
-        </li>`;
+  <li>
+    <a target="_blank" href="${lead.url}">${lead.url}</a>
+    ${lead.description ? `<p class="description">${lead.description}</p>` : ""}
+    <span class="timestamp">${lead.timestamp}</span>
+    <button class="delete-btn" data-url="${lead.url}">🗑️</button>
+  </li>`;
     });
     container.innerHTML += `</ul>`;
   }
+  const deleteButtons = container.querySelectorAll(".delete-btn");
+
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const urlToDelete = e.target.dataset.url;
+      const updatedLeads = leads.filter((lead) => lead.url !== urlToDelete);
+
+      saveLeads(updatedLeads);
+      renderLeads(updatedLeads, container);
+    });
+  });
 }
 
 export function renderCategoryOptions(categories, selectEl) {
