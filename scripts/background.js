25d381e2abeedbc4a424b2aa1022f86baa2aec80
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-10-13 10:31:44 +0200

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -9,8 +9,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (!myLeads.some((lead) => lead.url === url)) {
         myLeads.push({ url, category, description, timestamp, isFavorite });
 
-        chrome.storage.local.set({ myLeads });
+        chrome.storage.local.set({ myLeads }, () => {
+          console.log("Tab saved:", url);
+
+          sendResponse({ success: true });
+        });
+      } else {
+        sendResponse({ success: false, message: "Tab already saved" });
       }
     });
+
+    return true; // Keeps the message channel open for async response
   }
 });
